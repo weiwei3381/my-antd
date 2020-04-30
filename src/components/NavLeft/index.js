e02d80351d72e7d5c3eef8fdf59335ba9f17d77a
wowbat <weiwei3381@live.com>
2020-05-01 01:59:43 +0800

    实现左侧菜单栏

---
+++ src/components/NavLeft/index.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Menu, Icon } from 'antd'
+import './index.less'
+import MenuConfig from '../../resource/menuConfig'
+
+const SubMenu = Menu.SubMenu
+
+export default class NavLeft extends React.Component {
+  componentWillMount() {
+    const menuTreeNode = this.renderMenu(MenuConfig)
+    this.setState({
+      menuTreeNode,
+    })
+  }
+
+  renderMenu = (data) => {
+    return data.map((item) => {
+      if (item.children) {
+        return (
+          <SubMenu title={item.title} key={item.key}>
+            {this.renderMenu(item.children)}
+          </SubMenu>
+        )
+      }
+      return <Menu.Item key={item.key}>{item.title}</Menu.Item>
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="logo">
+          <img src="/assets/shield2.svg" alt="" />
+          <h1>文件管理系统</h1>
+        </div>
+        <Menu theme="dark">{this.state.menuTreeNode}</Menu>
+      </div>
+    )
+  }
+}
