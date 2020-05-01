ba0043aa7bd45d5a5bf577b6c5990318c2849cb3
wowbat <weiwei3381@live.com>
2020-05-01 10:18:14 +0800

    增加头部组件css基本样式

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { Row, Col } from 'antd'
+import './index.less'
 
 export default class Header extends React.Component {
+  componentWillMount() {
+    this.setState({
+      username: '小熊',
+    })
+  }
   render() {
     return (
-      <div>
-        <p>this is Header</p>
+      <div className="header">
+        <Row className="header-top">
+          <Col span={24}>
+            <span>欢迎，{this.state.username}</span>
+            <a href="#">退出</a>
+          </Col>
+        </Row>
+        <Row className="breadcrumb">
+          <Col span={4} className="breadcrumb-title">
+            首页
+          </Col>
+          <Col span={20} className="weather">
+            <span className="date">2020年5月1日</span>
+          </Col>
+        </Row>
       </div>
     )
   }
